29972784ba40af769f0d1d4ecf205f8880b69d5d
MrTiger <rajbajpai2004@gmail.com>
2022-09-22 22:19:48 +0530

    adding chapter-3's few solutions

---
+++ letUsC-3.c
@@ -0,0 +1,41 @@
+// SOLVED PROBLEMS
+// Problem 3.2
+// Input an integer and check wheather it's even or odd
+// #include <stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a;
+//     printf("Enter an integer\n");
+//     scanf("%d", &a);
+//     if (a % 2 == 0)
+//         printf("Entered integer is even\n");
+//     else
+//         printf("Entered integer is odd\n");
+//     return 0;
+// }
+
+// Finding whether input year is leap year or not
+/*
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int yr;
+    printf("Enter any year\n");
+    scanf("%d", &yr);
+    if (yr % 100 == 0)
+    {
+        if (yr % 400 == 0)
+            printf("Leap year\n");
+        else
+            printf("NOT a Leap year\n");
+    }
+    else
+    {
+        if (yr % 4 == 0)
+            printf("Leap year\n");
+        else
+            printf("NOT a Leap year\n");
+    }
+    return 0;
+}
+*/
